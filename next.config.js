2fc0b1fe7f952889cbd3dc3e2849f36bab95a6d1
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2026-03-15 16:27:33 -0300

    Use exact working configuration from my_profile repository

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  images: {
-    unoptimized: true
-  }
-}
+    output: "export",
+    reactStrictMode: true,
+    basePath: isProd ? '/Portfolio-Jeremias-Ahmed' : '',
+    assetPrefix: isProd ? '/Portfolio-Jeremias-Ahmed/' : '',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    }
+};
 
 module.exports = nextConfig
